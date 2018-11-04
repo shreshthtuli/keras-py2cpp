8187cde3c0c8850ddb79d60dbeb9482ee5f9b5c6
Shreshth Tuli <shreshthtuli@gmail.com>
2018-11-04 20:32:51 +0530

    Update yinshCoach.py

--- yinshCoach.py
+++ yinshCoach.py
@@ -10,8 +10,10 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras import backend as K
 from kerasify import export_model
 
-EPISODES = 10
+EPISODES = 100
 STATE_SIZE = 10
+BATCH_SIZE = 32
+experienceFile = "YinshExp.exp"
 
 # Deep Q-learning Agent
 class DQNAgent:
@@ -22,7 +24,7 @@ class DQNAgent:
         self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
-        self.learning_rate = 0.001
+        self.learning_rate = 0.00025
         self.model = self._build_model()
         self.reward = 0
 
@@ -39,22 +41,19 @@ class DQNAgent:
     def updateReward(self, newReward):
         self.reward = newReward
 
-    def remember(self, state):
-        self.memory.append(state)
+    def remember(self, state, next_states):
+        self.memory.append((state, next_states))
 
     def replay(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
         i = 0
         while i < len(minibatch):
             target = self.reward
-            state = minibatch[i]
+            state, next_states = minibatch[i]
             if i != (len(minibatch) - 1):
-              next_state = minibatch[i+1]
               target = self.reward + self.gamma * \
-                       np.amax(self.model.predict(next_state)[0])
-            target_f = self.model.predict(state)
-            target_f[0][action] = target
-            self.model.fit(state, target_f, epochs=1, verbose=0)
+                       np.amax(self.model.predict(next_states)[0])
+            self.model.fit(state, target, epochs=1, verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 		
@@ -62,30 +61,21 @@ class DQNAgent:
         export_model(self.model, 'yinsh.model')
 		
 
-
 if __name__ == "__main__":
     agent = DQNAgent(STATE_SIZE)
-    # Iterate the game
+    with open(experienceFile) as f:
+        # Parse reward of experience
+        reward = f.readline()
+        agent.updateReward(reward)
+        # Parse file for state and next state
+        state = f.readline()
+        next_states = f.readline()
+        agent.remember(state, next_states)
+
+    # Iterate the game experience
     for e in range(EPISODES):
-        # time_t represents each frame of the game
-        # Our goal is to keep the pole upright as long as possible until score of 500
-        # the more time_t the more score
-        for time_t in range(500):
-            # Decide action
-            action = agent.act(state)
-            # Advance the game to the next frame based on the action.
-            # Reward is 1 for every frame the pole survived
-            next_state = np.reshape(next_state, [1, 4])
-            # Remember the previous state, action, reward, and done
-            agent.remember(state)
-            # make next_state the new current state for the next frame.
-            state = next_state
-            # done becomes True when the game ends
-            # ex) The agent drops the pole
-            if done:
-                # print the score and break out of the loop
-                print("episode: {}/{}, score: {}"
-                      .format(e, EPISODES, time_t))
-                break
         # train the agent with the experience of the episode
-        agent.replay(32)
+        agent.replay(BATCH_SIZE)
+        print("episode: {}/{}, score: {}"
+        .format(e, EPISODES, agent.reward))
+        
