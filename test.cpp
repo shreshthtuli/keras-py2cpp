c22ff1e49dcdc0a6c44fa1fd27b2d737be92c359
Shreshth Tuli <shreshthtuli@gmail.com>
2018-11-04 15:00:58 +0530

    updated test.cpp

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include "keras/model.h"
+
+using keras::Model;
+using keras::Tensor;
+
+int main() {
+    // Initialize model.
+    auto model = Model::load("example.model");
+
+    // Create a 1D Tensor on length 10 for input data.
+    Tensor in{10};
+    in.data_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    // Run prediction.
+    Tensor out = model(in);
+    out.print();
+    return 0;
+}
